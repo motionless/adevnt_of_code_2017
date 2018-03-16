e8b91d3c03f173cbe42d0c87a559cea9f4dda46d
Henry Gallert <henry.gallert@gmail.com>
2018-03-16 22:42:29 +0100

    fix implementation & refactor the code

--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -2,14 +2,18 @@ defmodule DayOneTest do
   use ExUnit.Case
 
   test "get the inverse captcha" do
-    DayOne.input()
-    |> DayOne.inverse_captcha(2)
-    |> IO.puts
+    captcha =
+      InputValues.day_one_input()
+      |> DayOne.inverse_captcha(1)
+
+    assert captcha == 1203
   end
 
   test "get the inverse captcha part 2" do
-    DayOne.input()
-    |> DayOne.inverse_captcha(div(String.length(DayOne.input()), 2))
-    |> IO.puts()
+    captcha =
+      InputValues.day_one_input()
+      |> DayOne.inverse_captcha(div(String.length(InputValues.day_one_input()), 2))
+
+    assert captcha == 1146
   end
 end
